e52265a390af72290b0ec2337be766ceb8f53f05
nick-spirakis <137730498+nick-spirakis@users.noreply.github.com>
2023-10-18 14:57:57 -0500

    Added MainAcivity

---
+++ MainActivity.java
@@ -0,0 +1,44 @@
+package edu.lewisu.cs.spirakisnick.snstest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        //String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("TOKEN", token);
+                        //System.out.println(token);
+                        Toast.makeText(MainActivity.this, "Your device registration token is: " + token,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
